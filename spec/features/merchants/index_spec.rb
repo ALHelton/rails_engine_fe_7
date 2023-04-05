c853357f57868b91f00a8e5840b102fb1449afea
Andra Helton <andrahelton@gmail.com>
2023-04-05 17:58:56 -0600

    Add test for merchant index page

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -6,9 +6,17 @@ RSpec.describe "Merchant Index", type: :feature do
   end
 
   describe "As a visitor" do
-    it "List of merchants by name" do
+    it "I see a list of merchants by name" do
       visit merchants_path
       expect(page).to have_content("Merchants")
+      expect(page).to have_content("Hand-Spencer")
+    end
+
+    it "I click merchant's name, am routed to '/merchants/:id'" do
+      visit merchants_path
+      expect(page).to have_link("Hand-Spencer")
+      click_link "Hand-Spencer"
+      expect(current_path).to eq("/merchants/9")
     end
   end
 end
