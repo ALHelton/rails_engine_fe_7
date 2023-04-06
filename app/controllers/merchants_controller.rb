284f2b7058b6a64c2913b59599bb809c6b4121af
Andra Helton <andrahelton@gmail.com>
2023-04-05 18:00:57 -0600

    Add MerchantsController#index and #show

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,5 +1,14 @@
 class MerchantsController < ApplicationController
   def index
-    
+    response = Faraday.get("http://localhost:3000/api/v1/merchants")
+    @merchants = JSON.parse(response.body, symbolize_names: true)[:data]
+  end
+
+  def show
+    merchant_response = Faraday.get("http://localhost:3000/api/v1/merchants/#{params[:id]}")
+    @merchant = JSON.parse(merchant_response.body, symbolize_names: true)[:data]
+    merchant_items_response = Faraday.get("http://localhost:3000/api/v1/merchants/#{params[:id]}/items")
+    @merchant_items = JSON.parse(merchant_items_response.body, symbolize_names: true)[:data]
+    require 'pry'; binding.pry
   end
 end
